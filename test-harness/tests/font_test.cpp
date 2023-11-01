4428a1ef0822a23faed1c06c54124248e3779886
RS_Lappy\Pat <patrick.kingwill@roboticsystems.com.au>
2023-11-01 14:31:45 +1100

    Added some extra comments

--- test-harness/tests/font_test.cpp
+++ test-harness/tests/font_test.cpp
@@ -1,5 +1,6 @@
 /**
  * Tests for font file 
+ * - need to add a user_fonts folder that will add the inscope fonts to the lookup table 
  * - calling font_get_letter(&width,&height,&pointer,"nil","0","a") returns error
  * - calling font_get_letter(&width,&height,&pointer,"calibri","12","a") returns pointer to a 2D array containting
  * the letter bitmap and also the letters correct width and height
