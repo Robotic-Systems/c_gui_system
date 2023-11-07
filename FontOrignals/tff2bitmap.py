070866c18352e6787e503e35840dedec0fe4429d
RS_Lappy\Pat <patrick.kingwill@roboticsystems.com.au>
2023-11-08 07:50:00 +1100

    The python script is now able to output the bitmap file, it worked once and now is broken

--- FontOrignals/tff2bitmap.py
+++ FontOrignals/tff2bitmap.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Configuration
-font_size = 50  # Font size
+font_size = 19  # Font size
+font_name = "juipter"
 text_num = "0123456789"  # Text to convert to a bitmap
 text_lc = "abcdefghijklmnopqrstuvwxyz"
 text_uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -51,26 +52,79 @@ for i, char in enumerate(text_lc + text_uc + text_num +text_sym):
     text_width = text_bbox[2] - text_bbox[0]
     text_height = text_bbox[3] - text_bbox[1]
 
-    x = (output_width - text_width) / 2
+    # x = (output_width - text_width) / 2
     # y = (output_height - text_height) / 2
     y=0
+    x=0
 
     # Draw the text on the image
     draw.text((x, y), char, fill="white", font=font)
 
     # Update the corresponding depth (layer) in the bitmap_array
     image = image.convert('L')
-    plt.imshow(image)
-    plt.show()
+    # plt.imshow(image)
+    # plt.show()
     bitmap_array[:,: , i] = np.array(image)
 
-bitmap_array = bitmap_array[1:]
+# bitmap_array = bitmap_array[:,:,:1]
 
-bitmap_layer = bitmap_array[:,:,10]
-print(bitmap_array[:,:,10])
+# bitmap_layer = bitmap_array[:,:,10]
+# print(bitmap_array[:,:,10])
 # Create a grayscale image from the extracted layer
-bitmap_image = Image.fromarray(bitmap_layer, 'L')
+# bitmap_image = Image.fromarray(bitmap_layer, 'L')
 
-# Save the image to a file
-output_file = 'output_layer.png'
-bitmap_image.save(output_file)
+##
+# Creating C file 
+##
+# Define the dimensions of the 3D array
+num_characters = len(text_lc + text_uc + text_num + text_sym)
+font_bitmap = np.empty((font_size, output_width, num_characters), dtype=np.uint8)
+
+# Open a C file for writing
+with open("font_"+font_name+"_.h", "w") as c_file:
+    
+    c_file.write("#ifndef "+font_name.upper()+"_H \n#define "+font_name.upper()+"_H\n")
+    c_file.write("\n")
+    c_file.write("#include <stdint.h>\n")
+    c_file.write("\n")
+    # Write the C array declaration for the width array
+    c_file.write("uint8_t widthArray[{}] = {{\n".format(num_characters))
+
+    # Write the width array data to the C file
+    for i in range(num_characters):
+        c_file.write("  0x{:02X}".format(width_array[i]))
+        if i < num_characters - 1:
+            c_file.write(", ")
+        else:
+            c_file.write("\n")
+    
+    c_file.write("};\n\n")
+
+    # Write the C array declaration for the 3D bitmap array
+    c_file.write("uint8_t fontMap[{}][{}][{}] = {{\n".format(font_size, output_width, num_characters))
+
+    # Write the bitmap data to the C file
+    for i in range(num_characters):
+        c_file.write("  {\n")
+        for row in range(font_size):
+            c_file.write("    {")
+            for col in range(output_width):
+                c_file.write("0x{:02X}".format(font_bitmap[row, col, i]))
+                if col < output_width - 1:
+                    c_file.write(", ")
+            c_file.write("}")
+            if row < font_size - 1:
+                c_file.write(",\n")
+            else:
+                c_file.write("\n")
+        c_file.write("  }")
+        if i < num_characters - 1:
+            c_file.write(",\n")
+        else:
+            c_file.write("\n")
+
+    c_file.write("};\n")
+
+    c_file.write("#endif")
+
+print("C file 'font_bitmap.c' created successfully.")
