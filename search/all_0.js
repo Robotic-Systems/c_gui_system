4eba4295ca42b79b215768f909af9535d486fcb7
PatrickKingwill <PatrickKingwill@users.noreply.github.com>
2023-11-15 23:35:43 +0000

    deploy: cf309ea2a04d35b7d9bb41cbf89347408c583688

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['alignmentname_0',['alignmentName',['../structtext__alignment__opt__t.html#af2d45aecc10be4f755f4a8bcd81fbca0',1,'text_alignment_opt_t']]]
+  ['alignmentname_0',['alignmentname',['../structtext__alignment__opt__t.html#af2d45aecc10be4f755f4a8bcd81fbca0',1,'text_alignment_opt_t::alignmentName'],['../structtext__vert__alignment__opt__t.html#a12821e723bd8cda3fb602f6b4a03fcfc',1,'text_vert_alignment_opt_t::alignmentName']]]
 ];
--- search/classes_1.js
+++ search/classes_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['text_5falignment_5fopt_5ft_0',['text_alignment_opt_t',['../structtext__alignment__opt__t.html',1,'']]]
+  ['text_5falignment_5fopt_5ft_0',['text_alignment_opt_t',['../structtext__alignment__opt__t.html',1,'']]],
+  ['text_5fvert_5falignment_5fopt_5ft_1',['text_vert_alignment_opt_t',['../structtext__vert__alignment__opt__t.html',1,'']]]
 ];
